5600771b54e691a2e3cc0a37247f4423c02b146d
mokarahan <ozgurkarahan@yahoo.com>
2026-09-02 09:21:52 +0300

    Update load_csv.py
    Added discharge items

--- load_csv.py
+++ load_csv.py
@@ -18,7 +18,7 @@ args = parser.parse_args()
 # 4. Access and use the argument value
 print(f"Processing file: {args.filename}")
 
-# Define the path to your CSV file
+# Define the path to CSV file
 # (Can be a local file path or a direct web URL)
 file_path = args.filename
 line_number = args.line
@@ -32,8 +32,22 @@ try:
     print(df.info())
 
     # Display the first line_number rows of the DataFrame
-    print(f"--- First {line_number} Rows of Data ---")
-    print(df.loc[df['type'] == 'discharge'].head(line_number))
+    #print(f"--- First {line_number} Rows of Data ---")
+    #print(df.loc[df['type'] == 'discharge'].head(line_number))
+    
+    # Open only discharged rows
+    df_discharged = df.loc[df['type'] == 'discharge']
+    for f_index, f_row in df_discharged.iterrows():
+        print(f"Checking discharged record file: {f_row['filename']}")
+        df_disc_recs = pd.read_csv(f'./tmp/{f_row['filename']}')
+        
+        for d_in, d_row in df_disc_recs.iterrows():
+            print(f'Record at: {d_in} ',
+                  f'Voltage_Measured: {d_row['Voltage_measured']}, ',
+                  f'Current_measured: {d_row['Current_measured']}, ',
+                  f'Temperature_measured: {d_row['Temperature_measured']}',
+                  f'at: {d_row['Time']}',
+            )
 
 except FileNotFoundError:
     print(f"Error: The file at {file_path} was not found.")
