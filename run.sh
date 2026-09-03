ae2282f0e02a89d0aca6b8f29c9ae8ab3b2443ae
mokarahan <m.ozgur.karahan@gmail.com>
2026-09-03 16:22:18 +0300

    added new logs

--- run.sh
+++ run.sh
@@ -1,4 +1,21 @@
-source $HOME/.venv/bin/activate
-PROJECT_HOME=$HOME/Dev/doktora
+#!/bin/bash
 
-python3 $PROJECT_HOME/src/load_csv.py --filename $PROJECT_HOME/src/metadata.csv --data_dir $PROJECT_HOME/src/tmp --no-show_figures --no-generate_logs
+OS_TYPE=$(uname -s)
+
+case "$OS_TYPE" in
+    Linux*)
+        echo "Running on Linux"
+        PROJECT_HOME=$HOME/Dev/doktora
+        source $HOME/.venv/bin/activate
+        python3 $PROJECT_HOME/src/load_csv.py --filename $PROJECT_HOME/src/metadata.csv --data_dir $PROJECT_HOME/src/tmp --no-show_figures --no-generate_logs $1
+        ;;
+    Darwin*)
+        echo "Running on macOS"
+        # Insert Mac-specific commands here
+        ;;
+    *)
+        echo "Unknown Operating System: $OS_TYPE"
+        ;;
+esac
+
+
