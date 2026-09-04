871c16047bf0bf531053321f385a30394314d06e
mokarahan <m.ozgur.karahan@gmail.com>
2026-09-04 13:43:01 +0300

    Changed default run parameters

--- run.sh
+++ run.sh
@@ -7,13 +7,13 @@ case "$OS_TYPE" in
         echo "Running on Linux"
         PROJECT_HOME=$HOME/Dev/doktora
         source $HOME/.venv/bin/activate
-        python3 $PROJECT_HOME/src/LoadBatteryData.py --filename $PROJECT_HOME/src/metadata.csv --data_dir $PROJECT_HOME/src/tmp --no-show_figures --no-generate_logs $@
+        python3 $PROJECT_HOME/src/LoadBatteryData.py --filename $PROJECT_HOME/src/metadata.csv --data_dir $PROJECT_HOME/src/tmp --show_figures --generate_logs $@
         ;;
     Darwin*)
         echo "Running on macOS"
         PROJECT_HOME=$HOME/Dev/doktora
         source $HOME/path/to/venv/bin/activate
-        python3 $PROJECT_HOME/src/LoadBatteryData.py --filename $PROJECT_HOME/src/metadata.csv --data_dir $PROJECT_HOME/src/tmp --no-show_figures --no-generate_logs $@
+        python3 $PROJECT_HOME/src/LoadBatteryData.py --filename $PROJECT_HOME/src/metadata.csv --data_dir $PROJECT_HOME/src/tmp --show_figures --generate_logs $@
         # Insert Mac-specific commands here
         ;;
     *)
